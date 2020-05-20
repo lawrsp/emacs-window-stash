30fd6cdc12bbed4a6d9f61724ac86770105b8bff
lawr <lawrance.rsp@gmail.com>
2020-05-21 03:37:19 +0800

    add some comments

--- window-stash.el
+++ window-stash.el
@@ -1,5 +1,38 @@
-;;; window-stash.el --- Stash Windows -*- mode: emacs-lisp; lexical-binding: t -*-
+;;; window-stash.el --- Quickly Stash Windows. -*- lexical-binding: t -*-
 
+; Copyright © 2020 lawrsp <lawrance.rsp@gmail.com>
+
+;; Author: lawrsp <lawrance.rsp@gmail.com>
+;; URL: https://github.com/lawrsp/eamcs-window-stash
+;; Keywords: window, stash, convenience
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1") (avy "0.5.0") (ace-window "0.1.0"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This library provides the window-stash functionality 
+
+;;; Code:
 (require 'avy)
 (require 'ace-window)
 
